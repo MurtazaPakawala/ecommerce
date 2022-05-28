071e85118ffd46bd4743eefdb052ae0af009b0c7
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-29 00:27:28 +1000

    connecting to the mongo db server

--- backened/server.js
+++ backened/server.js
@@ -2,8 +2,12 @@ const app = require("./app");
 
 //requiring the dotenv
 const dotenv = require("dotenv");
+//importing the databasefunction
+const connectDatabse = require("./config/database");
 //config
 dotenv.config({ path: "backened/config/config.env" });
+//connecting to the database
+connectDatabse();
 app.listen(process.env.PORT, () => {
   console.log(`the server is up at the port ${process.env.PORT}`);
 });
