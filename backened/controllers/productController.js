bc171f608578f53cde689d8453a623f4f3ddf55b
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-29 16:20:21 +1000

    created the API for updating the product

--- backened/controllers/productController.js
+++ backened/controllers/productController.js
@@ -4,12 +4,30 @@ const Product = require("../models/productSchema");
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
-    sucess: true,
+    success: true,
     product,
   });
 };
 //getting all the products
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
-  res.status(200).json({ message: "the route is working fine", products });
+  res.status(200).json({ success: true, products });
+};
+//update all products
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({ success: true, product });
 };
